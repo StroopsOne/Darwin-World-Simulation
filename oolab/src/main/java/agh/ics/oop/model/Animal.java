e904864cb3f69a3e3ef44945cb0f8cce48932105
PatrykBlacha <pblacha@student.agh.edu.pl>
2024-12-29 19:18:46 +0100

    genetyka

--- oolab/src/main/java/agh/ics/oop/model/Animal.java
+++ oolab/src/main/java/agh/ics/oop/model/Animal.java
@@ -1,5 +1,9 @@
 package agh.ics.oop.model;
 
+import agh.ics.oop.model.properities.Genomes;
+
+import java.util.List;
+
 public class Animal implements WorldElement {
 
 
@@ -10,17 +14,52 @@ public class Animal implements WorldElement {
         return orientation;
     }
     private int energy;
-    private final int ReadyForKidsEnergy=100;       //ilość energii ustalona do rozmnażania
-    private boolean alive=true;
+    private final int ReproductionEnergy;       //ilość energii ustalona do rozmnażania
+    private final Genomes genes;
+    private int ageDays;
+    private int deathDay;
+    private final int mingeneMutation;
+    private final int maxgeneMutation;
+    private Animal parent1;
+    private Animal parent2;
+    private int childrenCount;
+    private final int parentingEnergy;  //energia zużywana przez posiadanie dzieci
 
     public Vector2d getPosition() {
 
         return position;
     }
-    public Animal(Vector2d vector, int energy){         //usunąłem deafult vector bo w projekcie nie ma już sensu, miejsce zwierzęcia i tak losowane
+    public Animal(Vector2d vector, int startEnergy, int geneSize, int reproductionEnergy,int mingeneMutation, int maxgeneMutation, int parentingEnergy){
         this.orientation = MapDirection.NORTH;
         this.position = vector;
-        this.energy=energy;
+        this.energy=startEnergy;
+        this.genes=new Genomes(geneSize);
+        this.ageDays=0;
+        this.deathDay=-1;       //czyli zwierzak żyje
+        this.ReproductionEnergy=reproductionEnergy;
+        this.mingeneMutation=mingeneMutation;
+        this.maxgeneMutation=maxgeneMutation;
+        this.parent1=null;
+        this.parent2=null;
+        this.childrenCount=0;
+        this.parentingEnergy=parentingEnergy;
+    }
+    //konstruktor dla tworzenia dzieci
+    public Animal(Vector2d position, int inheritedEnergy, Genomes genomes, int reproductionEnergy,int mingeneMutation, int maxgeneMutation,Animal parent1,Animal parent2, int parentingEnergy){
+        this.orientation = MapDirection.NORTH;
+        this.position = position;
+        this.energy=inheritedEnergy;
+        this.genes=genomes;
+        this.ageDays=0;
+        this.deathDay=-1;       //czyli zwierzak żyje
+        this.ReproductionEnergy=reproductionEnergy;
+        this.mingeneMutation=mingeneMutation;
+        this.maxgeneMutation=maxgeneMutation;
+        this.parent1=parent1;
+        this.parent2=parent2;
+        this.childrenCount=0;
+        this.parentingEnergy=parentingEnergy;
+
     }
 
     public int getEnergy() {
@@ -28,16 +67,35 @@ public class Animal implements WorldElement {
     }
 
     public boolean isAlive(){
-        return alive;
+        return deathDay==-1;
     }
 
     public void dayPasses(){
         energy=energy-1;
-        if (energy==0)  alive=false;
+        if (energy==0){
+            //przypisanie dnia śmierci, trzeba mieć czas zaimplementowany
+            //czas chyba do simulation
+        }else ageDays+=1;
+    }
+
+    public int getAge(){
+        return ageDays;
+    }
+
+    public int getDeathDay(){
+        return deathDay;
+    }
+
+    public Genomes getGenes(){
+        return genes;
+    }
+
+    public int GetNumberOfChildren(){
+        return childrenCount;
     }
 
     public void killedBySowoniedzwiedz(){
-        alive=false;
+        //przypisanie deathDay to samo co w dayPasses
     }
 
     public void plantEatten(int plantValue){
@@ -45,8 +103,25 @@ public class Animal implements WorldElement {
     }
 
     public boolean ReadyForKids(){
-        if (energy>ReadyForKidsEnergy)  return true;
+        if (energy>ReproductionEnergy)  return true;
         else return false;
+    }
+
+    // reprodukcja zwraca animal- dziecko
+    public Animal reproduce(Animal partner){
+        List<Integer> childGenes=this.genes.ChildGenes(this,partner);
+        Genomes childGenome=new Genomes(childGenes,this.mingeneMutation,this.maxgeneMutation);
+        this.animalChangeEnergy(parentingEnergy);
+        partner.animalChangeEnergy(parentingEnergy);
+        this.childrenCount+=1;      //dodanie dziecka do atrybutów rodziców
+        partner.childrenCount+=1;
+        return new Animal(this.position, 2*parentingEnergy,childGenome,this.ReproductionEnergy,this.mingeneMutation,this.maxgeneMutation,this,partner,this.parentingEnergy);
+    }
+
+    //dla bezpieczenj zmiany energi, bez wartości ujemnych
+    public void animalChangeEnergy(int delta){
+        this.energy+=delta;
+        if(this.energy<0)   energy=0;
     }
 
     public String toString(){
---
+++ oolab/src/main/java/agh/ics/oop/model/properities/Genomes.java
@@ -0,0 +1,71 @@
+package agh.ics.oop.model.properities;
+import agh.ics.oop.model.AbstractWorldMap;
+import agh.ics.oop.model.Animal;
+
+import java.util.*;
+
+public class Genomes {
+    private final List<Integer>genes;
+
+    public Genomes(int genomeSize){
+        this.genes=new ArrayList<>();
+        Random random= new Random();
+        for (int i=0;i<genomeSize;i++){
+            this.genes.add(random.nextInt(8));
+        }
+    }
+    public Genomes(List<Integer>genes, int minGeneMutation, int maxGeneMutation){
+        this.genes=genes;
+        Mutate(genes, minGeneMutation, maxGeneMutation);
+    }
+
+    public List<Integer>getGenes(){
+        return genes;
+    }
+
+    public List<Integer>ChildGenes(Animal mom, Animal dad){     //mom, dad umowne, którsze nazwy od firstParent
+        Animal strongerParent = mom.getEnergy() > dad.getEnergy() ? mom : dad;
+        Animal weakerParent = mom.getEnergy() > dad.getEnergy() ? dad : mom;
+
+        double totalEnergy= strongerParent.getEnergy()+ weakerParent.getEnergy();
+        double percentageStrongerParent= strongerParent.getEnergy()/totalEnergy;        //weakerParent nie potrzebne, w praktyce prawdopodobieństwu genu tego rodzica
+        Genomes strongerGenes= strongerParent.getGenes();     //zmienne dodane dla estetyki kodu
+        Genomes weakerGenes= weakerParent.getGenes();
+
+        List<Integer>childGenes=new ArrayList<>();
+        Random random=new Random();
+        for (int i = 0; i < strongerGenes.getGenes().size(); i++) {
+            if (random.nextDouble() < percentageStrongerParent) {       //losowość z prawdopodobieństwem pojedynczy gen
+                childGenes.add(strongerGenes.getGenes().get(i));
+            } else {
+                childGenes.add(weakerGenes.getGenes().get(i));
+            }
+        }
+        return childGenes;
+
+    }
+
+    public void Mutate(List<Integer>genes,int mingeneMutation, int maxgeneMutation){
+        List<Integer>mutatedGenes= new ArrayList<>();
+        int numberOfMutations;
+        Random random= new Random();
+        if (mingeneMutation==maxgeneMutation)   numberOfMutations=mingeneMutation;
+        else    numberOfMutations=random.nextInt(maxgeneMutation - mingeneMutation) + mingeneMutation;
+
+        Set<Integer> mutatedIndices = new HashSet<>(); // Zbiór zmutowanych genów
+        for (int i = 0; i < numberOfMutations; i++) {
+            int randomIndex;
+
+            // Znajdywanie losowego, jeszcze nie zmutowanego genu
+            do {
+                randomIndex = random.nextInt(genes.size());
+            } while (mutatedIndices.contains(randomIndex));
+
+            mutatedIndices.add(randomIndex);
+
+            genes.set(randomIndex, random.nextInt(8));
+        }
+
+    }
+
+}
